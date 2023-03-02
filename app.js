328f199973e5db5b08ffdca96386115087cd172a
Shreyanshu07 <royshreyanshu1@gmail.com>
2023-03-02 18:01:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const router=require('./Routes/roomRoutes')
+const cors=require('cors')
+const app=express();
+const dotenv=require('dotenv');
+dotenv.config({path:'./.env'})
+const DB=process.env.DATABASE_URL;
+//MiddleWare
+// app.use('/',(req,res)=>{
+//     res.send("This is starting application")
+// })
+app.use(express.json())
+app.use(cors())
+app.use('/rooms',router)
+
+const URL=
+mongoose.connect(DB,{
+    
+})
+.then(()=>console.log("Successfully Connected"))
+.then(()=>{
+    app.listen(5000);
+}).catch((err)=>console.log(err))
